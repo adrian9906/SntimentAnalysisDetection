2f95a388640e2138a706e9a01ae7c1b3b48dc62e
Adrian Fernandez <adriandfl99@gmail.com>
2023-12-31 00:18:33 -0500

    Audio to text added

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import json
 from IPython.display import display
 from sklearn.metrics import accuracy_score
 from tabulate import tabulate
+from Audio2Text.audio_text import voiceToText
 from ExtractPDFInfo.pdfExtractor import extractText
 from SentimentAlgorithm.nltk.sentimentAnalysis import nltkSentimentAnalysis
 from SentimentAlgorithm.textBlob.sentimentDetection import sentimentTextBlob
@@ -24,22 +25,30 @@ def sentimenDetectMain(data):
 
 # print(df2)
 
-datatext = []
-with open ('D:/apps/Proyectos Freelance/SntimentAnalysisDetection/dataset/Sentiment_EN_WorldCup.json',mode='r') as file:
-    data = json.load(file)
+# Tweet dataset
+# datatext = []
+# with open ('D:/apps/Proyectos Freelance/SntimentAnalysisDetection/dataset/Sentiment_EN_WorldCup.json',mode='r') as file:
+#     data = json.load(file)
 
-datatext = [x['tweet_text'] for x in data]
-dataLabel = [x['sentiment'].lower() for x in data]
+# datatext = [x['tweet_text'] for x in data]
+# dataLabel = [x['sentiment'].lower() for x in data]
 
-df, df2 = sentimenDetectMain(datatext)
+# df, df2 = sentimenDetectMain(datatext)
 
-labelTextBlob = [x for x in df2['sentiment']]
-labelNLTK = [x for x in df['sentiment']]
+# labelTextBlob = [x for x in df2['sentiment']]
+# labelNLTK = [x for x in df['sentiment']]
 
 
-accuracy = accuracy_score(labelNLTK, dataLabel)
-accuracy2 = accuracy_score(labelTextBlob, dataLabel)    
+# accuracy = accuracy_score(labelNLTK, dataLabel)
+# accuracy2 = accuracy_score(labelTextBlob, dataLabel)    
 
-print(accuracy)
+# print(accuracy)
 
-print(accuracy2)
+# print(accuracy2)
+
+audio = [voiceToText('es')]
+
+if audio != 'still waiting':
+    df, df2 = sentimenDetectMain(audio)
+    print(df)
+    print(df2)    
